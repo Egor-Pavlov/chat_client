a90d9d0b462bb05f19466ab2a61573b2fe969a83
Егор Павлов <egor12200@gmail.com>
2024-08-08 10:54:11 +0700

    Обновил класс Message

--- src/client/model/Message.java
+++ src/client/model/Message.java
@@ -1,40 +1,30 @@
 package client.model;
+
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Message {
-    private String username;
-    private String text;
-    private ZonedDateTime timestamp;
+    private final String username;
+    private final String text;
+    private final ZonedDateTime timestamp;
 
     public Message(String username, String text, ZonedDateTime timestamp) {
-        this.username = username;
-        this.text = text;
-        this.timestamp = timestamp;
+        this.username = Objects.requireNonNull(username, "Username cannot be null");
+        this.text = Objects.requireNonNull(text, "Text cannot be null");
+        this.timestamp = Objects.requireNonNull(timestamp, "Timestamp cannot be null");
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getText() {
         return text;
     }
 
-    public void setText(String text) {
-        this.text = text;
-    }
-
     public ZonedDateTime getTimestamp() {
         return timestamp;
-    }
-
-    public void setTimestamp(ZonedDateTime timestamp) {
-        this.timestamp = timestamp;
     }
 
     public String toJson() {
@@ -44,10 +34,14 @@ public class Message {
     }
 
     public static Message fromJson(String json) {
-        String username = json.split("\"username\":\"")[1].split("\"")[0];
-        String text = json.split("\"text\":\"")[1].split("\"")[0];
-        String timestampStr = json.split("\"timestamp\":\"")[1].split("\"")[0];
-        ZonedDateTime timestamp = ZonedDateTime.parse(timestampStr, DateTimeFormatter.ISO_ZONED_DATE_TIME);
-        return new Message(username, text, timestamp);
+        try {
+            String username = json.split("\"username\":\"")[1].split("\"")[0];
+            String text = json.split("\"text\":\"")[1].split("\"")[0];
+            String timestampStr = json.split("\"timestamp\":\"")[1].split("\"")[0];
+            ZonedDateTime timestamp = ZonedDateTime.parse(timestampStr, DateTimeFormatter.ISO_ZONED_DATE_TIME);
+            return new Message(username, text, timestamp);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Invalid JSON format", e);
+        }
     }
 }
